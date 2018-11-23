5915ad703d0094c6b952b77e6434ec3447d38695
Nerea Bejar <NBM883@hotmail.com>
2018-11-23 14:18:01 +0100

    Commit of the regression data treated

--- Set_Training_Test.m
+++ Set_Training_Test.m
@@ -10,7 +10,7 @@ binary_test_features = data_matrix_per(1:132,1:floor(size(data_matrix_per,2)/5))
 binary_test_labels = data_matrix_per(133,1:floor(size(data_matrix_per,2)/5));
 binary_training_features = data_matrix_per(1:132,(floor(size(data_matrix_per,2)/5)+1):end);
 binary_training_labels = data_matrix_per(133,(floor(size(data_matrix_per,2)/5)+1):end);
- save('binary_test_features.mat','binary_test_features')
+save('binary_test_features.mat','binary_test_features')
 save('binary_training_features.mat','binary_training_features')
 save('binary_training_labels.mat','binary_training_labels')
 save('binary_test_labels.mat','binary_test_labels')
